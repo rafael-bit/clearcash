d46832012ec5cc3ab7eefa1e65b382f47d391462
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2024-01-29 19:28:00 -0300

    fist pages

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,58 @@
+export default function Terms() {
+	return (
+		<main className="p-6 max-w-4xl mx-auto">
+			<h1 className="text-2xl pb-10">Terms and Conditions</h1>
+			<p>Welcome to the Terms of Service and Privacy Policy of ClearCash, a financial control app developed by ClearCash. By using the services provided by this app, you agree to the following terms and policies. Please read carefully before using our services.</p>
+
+			<h1 className="text-lg p-4"><b>Terms of Service</b></h1>
+			
+			<ul className="list-decimal">
+				<li>
+					<b>Acceptance of Terms: </b>
+					By using ClearCash, you agree to these Terms of Service. If you do not agree to these terms, please do not use the application.
+				</li>
+				<li>
+					<b>Use of the App: </b>
+					ClearCash is intended exclusively for personal financial control. Commercial use or any activity that violates local, national, or international laws is strictly prohibited.
+				</li>
+				<li>
+					<b>User Responsibilities: </b>
+					You are responsible for maintaining the confidentiality of your login information. Any activity conducted through your account is your responsibility.
+				</li>
+				<li>
+					<b>Changes to the Terms: </b>
+					We reserve the right to modify these Terms of Service at any time. Significant changes will be communicated through the app or other appropriate means.
+				</li>
+				<li>
+					<b>Account Suspension or Termination: </b>
+					We may suspend or terminate your account at our discretion if we believe you have violated these Terms of Service or are engaged in fraudulent activities.
+				</li>
+			</ul>
+
+			<h1 className="text-lg p-4"><b>Privacy Policy</b></h1>
+
+				1. **Information Collected:**
+				We collect limited personal information necessary to provide our services, such as account information, financial transactions, and other relevant details.
+
+				2. **Use of Information:**
+				The information collected is used solely to provide ClearCash services. We do not share your information with third parties without your consent, except as required by law.
+
+				3. **Security:**
+				We implement security measures to protect your information. However, please note that no system is entirely fail-proof.
+
+				4. **Cookies and Similar Technologies:**
+				We may use cookies and similar technologies to enhance the user experience and collect information for analysis.
+
+				5. **Changes to the Privacy Policy:**
+				We reserve the right to modify this Privacy Policy at any time. Significant changes will be communicated through the app or other appropriate means.
+
+				By using ClearCash, you agree to this Privacy Policy and the processing of your information as described herein.
+
+				If you have any questions or concerns, please contact us at [support email].
+
+				Effective Date: 30/01/2024
+
+				Last Updated: 15/03/2024
+		</main>
+	)
+}
