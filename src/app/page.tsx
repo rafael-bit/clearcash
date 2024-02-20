19e8f2328b1d601e031f420122fc351f193f79d6
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2024-02-20 16:44:10 -0300

    try correct

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Login() {
     };
 
     try {
-      const response = await fetch('http://localhost:8080/api/services', {
+      const response = await fetch('http://localhost:8080/api/services/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -29,7 +29,15 @@ export default function Login() {
       }
 
       const responseData = await response.json();
-      setFeedback('Login successful!');
+
+      if (response.status === 404) {
+        setFeedback('User not found. Please check your credentials.');
+      } else if (response.status === 200) {
+        setFeedback('Login successful!');
+
+      } else {
+        setFeedback('Unknown error. Please try again.');
+      }
     } catch (error: any) {
       setFeedback(error.message);
       console.error(error);
@@ -49,8 +57,8 @@ export default function Login() {
       <section className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <Link href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-              <img className="w-8 h-8 mr-2" src="/icon-init.png" alt="logo" />
-              ClearCash
+            <img className="w-8 h-8 mr-2" src="/icon-init.png" alt="logo" />
+            ClearCash
           </Link>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
