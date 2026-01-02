ea8b687434d788694095b8b49b8b140491365b08
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2026-01-02 14:42:10 -0300

    feat: update Next.js configuration for Prisma integration and add engine type to schema

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@prisma/client');
+    }
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
 };
 
 export default nextConfig;
