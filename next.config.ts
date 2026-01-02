4e8dd0b0a5fa52522f9b8b89f7a68789662fd9a1
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2026-01-02 14:52:03 -0300

    feat: enhance Prisma configuration with additional binary target and logging options

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['@prisma/client'],
+  serverExternalPackages: ['@prisma/client', 'prisma'],
 };
 
 export default nextConfig;
